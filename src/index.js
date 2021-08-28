a1f62554e2befdf7fb3492fd25a7866d89b906b6
Pavel Kuvshinov <kuvsh1noffpavel@mail.ru>
2021-08-29 00:37:02 +0300

    feat: finished task expression calculator
    Finished task expression calculator. All tests pasted.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,75 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+
+  expr = expr.replace(/\*/g, ' * ');
+  expr = expr.replace(/\//g, ' / ');
+  expr = expr.replace(/\+/g, ' + ');
+  expr = expr.replace(/-/g, ' - ');
+  expr = expr.replace(/\(/g, ' ( ');
+  expr = expr.replace(/\)/g, ' ) ');
+  expr = expr.trimLeft().trimRight();
+
+  while (expr.includes('  ')) {
+    expr = expr.replace('  ', ' ');
+  }
+
+  let exprArr = expr.split(' ');
+  let count = 0;
+
+  for (const value of exprArr) {
+    if (value === '(') count++;
+    if (value === ')') count--;
+  }
+
+  if (count) throw new Error('ExpressionError: Brackets must be paired');
+
+  let openBracketPos = 0;
+  let closeBracket = 0;
+
+  for (let i = 0; i < exprArr.length; i++){
+    if (exprArr[i] == '('){
+      openBracketPos = i;
+    } else if (exprArr[i] == ')'){
+      closeBracket = i;
+
+      let mem = calculatorExprArr(exprArr.slice(openBracketPos + 1, closeBracket));
+      exprArr.splice(openBracketPos, closeBracket - openBracketPos + 1, mem);
+      i = -1;
+    }
+  }
+    
+  return calculatorExprArr(exprArr);
+
+  function calculatorExprArr(arr){
+  
+    const operator = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      '*': (a, b) => a * b,
+      '/': (a, b) => a / b,
+    }
+  
+    function calculator(op1, op2) {
+      for (let i = 0; i <= arr.length - 1; i++){
+        if(arr[i] === op1 || arr[i] === op2) {
+          if (arr[i] === '/' && arr[i+1] == 0){
+              throw new Error('TypeError: Division by zero.');
+          }
+          arr[i] = operator[arr[i]](+arr[i-1],+arr[i+1]);
+          arr.splice(i-1, 3, arr[i]);
+          i--;
+        }
+      }
+    }
+    
+    calculator('/', '*');
+    calculator('+', '-');
+
+    return arr[0];
+  
+  }
+
 }
 
 module.exports = {
